1d3a58902b95efaa90fd9db114513f73bc2b648a
Sergey Vasilyev <kosh.goo@gmail.com>
2019-01-17 00:08:14 +0300

    php

---
+++ script/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#header__btn').click(function () {
+        $('#popupContainer').css('display', 'flex');
+        $('body').css('overflow', 'hidden');
+    })
+
+    $('#popupContainer').click(function(event){
+        if (event.target === this) {
+            $('#popupContainer').css('display', 'none');
+            $('body').css('overflow', '');
+        }
+    })
+
+    $('#popup__submit').click(function(){
+        var name = $('#popup__name').val();
+        if(name == '') {
+            alert('Введите имя');            
+        } else {
+            $.post('/api.php', {'name' : name}, function(response){
+                alert(response);
+            });
+        }
+    });
+});

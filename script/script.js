62c25ce95ac5fedaeb0085a930e8f5e66d87cb5a
Sergey Vasilyev <kosh.goo@gmail.com>
2019-01-17 01:34:11 +0300

    FINAL

--- script/script.js
+++ script/script.js
@@ -1,25 +1,29 @@
 $(document).ready(function () {
-    $('#header__btn').click(function () {
+    $('.ordercall__btn').click(function () {
         $('#popupContainer').css('display', 'flex');
         $('body').css('overflow', 'hidden');
     })
 
-    $('#popupContainer').click(function(event){
+    $('#popupContainer').click(function (event) {
         if (event.target === this) {
             $('#popupContainer').css('display', 'none');
             $('body').css('overflow', '');
         }
     })
 
-    $('#popup__submit').click(function(){
+    $('#popup__submit').click(function () {
         var name = $('#popup__name').val();
         var phone = $('#popup__phone').val();
-        if(name == '') {
+        if (name == '') {
             alert('Введите имя');
         } else {
-            $.post('script/api.php', {'name' : name, 'phone' : phone}, function(response){
+            $.post('script/api.php', { 'name': name, 'phone': phone }, function (response) {
                 alert(response);
             });
+            $('#popup__name').val('');
+            $('#popup__phone').val('');
+            $('#popupContainer').css('display', 'none');
+            $('body').css('overflow', '');
         }
     });
 });
